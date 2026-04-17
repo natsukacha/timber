438adbdadf4ba60ff8daa54d9bddb86a4106cd94
natsukacha <s1611537@klis.tsukuba.ac.jp>
2026-04-18 08:32:49 +0900

    WIP:one_pipline,ml_pipeline:change one_pipeline.py mlflow.start_run() cell for execuatble , not excutable load model and predict

--- work/ml_pipeline.py
+++ work/ml_pipeline.py
@@ -387,6 +387,11 @@ class FullPipelineModel(mlflow.pyfunc.PythonModel):
 
     def __init__(self, pipe):
         self.pipe = pipe
+    
+    def load_context(self, context):
+        import pickle
+        with open(context.artifacts["pipe"], "rb") as f:
+            self.pipe = pickle.load(f)
 
     def predict(self, context, model_input):
         return self.pipe.predict(model_input)
